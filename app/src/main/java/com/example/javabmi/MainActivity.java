cfffd39affc449bfcd62f93ebb8f47d0abe4f6ca
Jack Lee <jack312505@gmail.com>
2022-02-18 18:43:35 +0800

    javaBMIinit javaBMIlayout javaBMItoast

--- app/src/main/java/com/example/javabmi/MainActivity.java
+++ app/src/main/java/com/example/javabmi/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.javabmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.javabmi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+    public void calculate(View view) {
+        Float weight = Float.valueOf(binding.edWeight.getText().toString());
+        Float height = Float.valueOf(binding.edHeight.getText().toString());
+        Float bmi = weight / (height * height);
+        Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show();
     }
 }
