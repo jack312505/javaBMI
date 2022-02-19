c31e4e2d212c827eafa59ebef012e282547f9aa4
Jack Lee <jack312505@gmail.com>
2022-02-19 18:06:47 +0800

    javaBMIinit javaBMIlayout javaBMItoast javaBMIhelp

--- app/src/main/java/com/example/javabmi/MainActivity.java
+++ app/src/main/java/com/example/javabmi/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.javabmi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,8 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-    }
+        binding.bHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder help = new AlertDialog.Builder(MainActivity.this);
+                help.setTitle("BMI說明");
+                help.setMessage("體重(kg)/身高的平方(m)");
+                help.setPositiveButton("OK", null);
+                help.show();
+            }
+        });
+        }
     public void calculate(View view) {
+        if(binding.edWeight.getText().toString().equals(""))
+            binding.edWeight.setText("0");
+        if(binding.edHeight.getText().toString().equals(""))
+            binding.edHeight.setText("0");
         Float weight = Float.valueOf(binding.edWeight.getText().toString());
         Float height = Float.valueOf(binding.edHeight.getText().toString());
         Float bmi = weight / (height * height);
